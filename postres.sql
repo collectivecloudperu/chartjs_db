a8461b774712aeeecad062ac976da2129fea2701
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2019-04-01 09:52:47 -0500

    Update postres.sql

--- postres.sql
+++ postres.sql
@@ -32,22 +32,20 @@ CREATE TABLE `postres` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `stock` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `precio` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `fecha_creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `fecha_actualizacion` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+  `precio` varchar(255) COLLATE utf8_unicode_ci NOT NULL,  
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `postres`
 --
 
-INSERT INTO `postres` (`id`, `nombre`, `stock`, `precio`, `fecha_creacion`, `fecha_actualizacion`) VALUES
-(1, 'Mazamorra Morada', '50', '3.00', '2018-05-30 12:27:57', '0000-00-00 00:00:00'),
-(3, 'Torta de Chocolate', '35', '5.00', '2018-05-30 07:31:59', '2018-05-30 07:31:59'),
-(4, 'Pie de Manzana', '45', '4.50', '2018-07-20 02:55:45', '0000-00-00 00:00:00'),
-(5, 'Arroz con Leche', '36', '2.50', '2018-08-05 04:04:38', NULL),
-(6, 'Gelatina de Fresa', '60', '1.50', '2018-08-05 04:05:34', NULL),
-(7, 'Suspiro a la LimeÃ±a', '26', '6.00', '2018-08-05 04:11:17', NULL);
+INSERT INTO `postres` (`id`, `nombre`, `stock`, `precio`) VALUES
+(1, 'Mazamorra Morada', '50', '3.00'),
+(3, 'Torta de Chocolate', '35', '5.00'),
+(4, 'Pie de Manzana', '45', '4.50'),
+(5, 'Arroz con Leche', '36', '2.50'),
+(6, 'Gelatina de Fresa', '60', '1.50'),
+(7, 'Suspiro a la LimeÃ±a', '26', '6.00');
 
 --
 -- Índices para tablas volcadas
